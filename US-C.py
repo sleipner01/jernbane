4e58746857e66a31e6de09a18513eccc486be5da
magnusouren <magnut0203@gmail.com>
2023-03-21 18:38:59 +0100

    refakturert us-c

--- US-C.py
+++ US-C.py
@@ -1,5 +1,4 @@
-import sqlite3;
-import re;
+import sqlite3
 
 con = sqlite3.connect('jernbanen.db')
 
@@ -20,22 +19,24 @@ def getTrainsPerDayPerStation(date, station):
                     JOIN Togruteforekomst ON Togruteforekomst.rutenummer = Togrute.rutenummer
                     WHERE Jernbanestasjon.navn = ? AND Togruteforekomst.dato = ?
                 """, (station, date))
-    
+
     return res.fetchall()
 
-def printResult(res,date,station):
+
+def printResult(res, date, station):
     values = []
     for value in res:
         values.append(value[0])
-    
+
     print("\nRuter som går fra " + station + " den " + date + ":")
-    
-    if(len(values) == 0):
+
+    if (len(values) == 0):
         print("Ingen ruter denne dagen")
         return
-    
+
     for value in values:
         print(value)
+
 
 def dayToDate(day):
     dayConverter = {
@@ -47,48 +48,44 @@ def dayToDate(day):
         "lør": "2023-04-08",
         "søn": "2023-04-09"
     }
-    return dayConverter[day]
+    return dayConverter.get(day, "undefined")
 
 
-
-def main ():
+def main():
     stations = getStations()
 
     print("Velkommen til Jernbanen, her kan du se hvilke ruter som går fra en stasjon på en gitt ukedag\n")
     print("Dag på format (man, tir, ons, tor, fre, lør, søn))\n")
-
     print("Stasjoner: ")
     print(", ".join(stations))
-
     print("\n(Skriv q for å avslutte)")
 
     res = ""
 
     while res != "q":
-        res = input("Skriv inn ukedag og stasjon separert med komma (,) uten mellomrom: ")
+        res = input(
+            "Skriv inn ukedag og stasjon separert med komma (,) uten mellomrom: ")
 
-        if(len(res.split(",")) != 2):
+        if (len(res.split(",")) != 2):
             print("Du må skrive inn dato og stasjon")
             continue
+
         day = res.split(",")[0]
         station = res.split(",")[1]
 
-        date = dayToDate(day)
+        date = dayToDate(day) # converts day to date since we only have dates in the database and every week is equal
 
-        x = re.search(r"\d{4}-\d{2}-\d{2}", date)
-        if(x == None):
-            print("Datoen er ikke på riktig format (yyyy-mm-dd)")
+        if (date == "undefined"):
+            print("Ugyldig dag")
             continue
+
         if (station not in stations):
             print("Stasjonen finnes ikke")
             continue
-        
+
         printResult(getTrainsPerDayPerStation(date, station), date, station)
 
         res = "q"
 
-    # ukedag og ikke dato
-
 
 main()
-# print(getTrainsPerDayPerStation("Trondheim", "2023-04-03"))
