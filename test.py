1b291e9d547e9fb6ad687cf34ca2a0ece786db76
magnusouren <magnut0203@gmail.com>
2023-03-24 19:35:38 +0100

    cleanup

--- test.py
+++
@@ -1,5 +0,0 @@
-foo = "433221"
-
-res = "".join(dict.fromkeys(foo))
-
-print(res == "4321")
--- tickets.sql
+++
@@ -1,6 +0,0 @@
--- Henter billetter
-
-SELECT Togrute.rutenummer as Rute, Billett.avgangsDato as avreise, VognerPaaVognOppsett.rekkefolge, Billett.plassNr FROM Togrute
-JOIN Togruteforekomst ON Togrute.rutenummer = Togrute.rutenummer
-JOIN Billett ON Togrute.rutenummer = Billett.rutenummer
-JOIN VognerPaaVognOppsett ON VognerPaaVognOppsett.vognId = Billett.vognId
