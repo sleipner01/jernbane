f9c81ffe7c594691f22024736ea1d5aeacaa75c4
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-03-24 14:44:44 +0100

    added some confirmation to customer-reg

--- US-E.py
+++ US-E.py
@@ -6,12 +6,19 @@ con = sqlite3.connect('jernbanen.db')
 
 def registerUser():
     cur = con.cursor()
-    fornavn = input("Navn: ")
+    fornavn = input("Fornavn: ")
     etternavn = input("Etternavn: ")
     email = input("Email: ")
-    tlfNr = input("Telefonnummer: ")
+    tlfNr = input("Telefonnummer (8 siffer): ")
     cur.execute("INSERT INTO Kunde (fornavn, etternavn, email, tlfNr) VALUES (?, ?, ?, ?)", (fornavn, etternavn, email, tlfNr))
     con.commit()
+
+    getNewlyRegisteredUser()
+
+def getNewlyRegisteredUser():
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM Kunde WHERE kundenummer = (SELECT MAX(kundenummer) FROM Kunde)").fetchone()
+    print("Registrert bruker: " + res[1] + " " + res[2] + " med kundenummer " + str(res[0]))
 
 registerUser()
 con.close()
